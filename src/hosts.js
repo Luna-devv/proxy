a63d6199da7b405bf5a1e20116c5b5de2000d9f7
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-02-20 14:50:23 +0100

    Update hosts.js

--- src/hosts.js
+++ src/hosts.js
@@ -11,6 +11,10 @@ module.exports = {
         target: 5000,
         type: "WEB"
     },
+    "console.waya.one": {
+        target: 5001,
+        type: "WEB"
+    },
     "api.waya.one": {
         target: 4000,
         type: "WEB"
@@ -19,16 +23,44 @@ module.exports = {
         target: 4001,
         type: "WEB"
     },
+    "api.losti.xyz": {
+        target: 4002,
+        type: "WEB"
+    },
     "ptb.waya.one": {
         target: "https://www.waya.one",
         type: "REDIRECT"
     },
     "heartbeat-radio.live": {
-        target: 1081,
+        target: 3000,
         type: "WEB"
     },
     "azura.heartbeat-radio.live": {
-        target: 1081,
+        target: 9998,
         type: "WEB"
-    }
+    },
+    "stream.heartbeat-radio.live": {
+        target: "0.0.0.0:9998/radio/8000/radio.mp3",
+        type: "MEDIA"
+    },
+    "panel.heartbeat-radio.live": {
+        target: 7762,
+        type: "WEB"
+    },
+    "docker-dal1-internal.tayronm.tech": {
+        target: 9443,
+        type: "WEB"
+    },
+    "dj.heartbeat-radio.live": {
+        target: 8005,
+        type: "WEB"
+    },
+    "api.heartbeat-radio.live": {
+        target: 25571,
+        type: "WEB"
+    },
+    "heartbeatradio.net": {
+        target: 3000,
+        type: "WEB"
+    },
 }
