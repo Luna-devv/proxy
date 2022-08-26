32bf29023b1176d130a4cd1c031785de4223857a
Lunish <luni.siaa@gmail.com>
2022-08-26 19:22:14 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -69,9 +69,11 @@ export async function requestManager(req, res) {
 };
 
 export async function onError(err, req, res) {
-    res.writeHead(500, {
-        'Content-Type': 'text/html'
-    });
-    const html_500 = fs.readFileSync(path.join(__dirname, '../html/500.html'), 'utf-8')
-    return res.end(html_500.replace(/{host}/g, req.headers.host));
+    try {
+        res.writeHead(500, {
+            'Content-Type': 'text/html'
+        });
+        const html_500 = fs.readFileSync(path.join(__dirname, '../html/500.html'), 'utf-8')
+        return res.end(html_500.replace(/{host}/g, req.headers.host));
+    } catch { return }
 }
