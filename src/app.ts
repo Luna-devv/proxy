bb94c2e24e69014d28306ebea1607b0397ef816c
Lunish <luni.siaa@gmail.com>
2022-10-19 20:37:30 +0200

    app logger reset colors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ import { requestManager, proxy, onError } from './server';
 
 proxy.on('error', onError);
 http.createServer(requestManager).listen(Config.port.http, () => {
-    console.log(`\x1b[33mHTTP (server) running on port ${Config.port.http}`);
-    console.log(`\x1b[33mProxy (server) running on port ${Config.port.proxy}`);
+    console.log(`\x1b[33mHTTP (server) running on port ${Config.port.http}\x1b[0m`);
+    console.log(`\x1b[33mProxy (server) running on port ${Config.port.proxy}\x1b[0m`);
 });
