719c598d2ec4c354e958a566b88b84a7da569b46
Luna <luni.siaa@gmail.com>
2023-04-15 23:49:06 +0200

    fix server upgrade websocket

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { http, ws, error, proxy } from './handlers';
 proxy.on('error', error.send);
 const server = node_proxy.createServer(http.handle);
 
-server.on('upgrade', (req, socket, head) => ws.handle);
+server.on('upgrade', ws.handle);
 
 server.listen(Config.port.http, () => {
     console.log(`\x1b[33mHTTP (server) running on port ${Config.port.http}\x1b[0m`);
